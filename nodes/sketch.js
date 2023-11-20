3b854317b27402f3d59280fedcf8173a723723ee
Honeybee4642 <tildenbruins@gmail.com>
2023-11-20 11:33:41 -0600

    Completed Demo

--- nodes/sketch.js
+++ nodes/sketch.js
@@ -1,4 +1,4 @@
-
+let points =[];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -6,6 +6,13 @@ function setup() {
 
 function draw() {
   background(220);
+  for(let point of points){
+    point.connectTo(points);
+    point.update();
+  }
+  for(let point of points){
+    point.display();
+  }
 }
 function mousePressed(){
   let thePoint = new MovingPoint(mouseX, mouseY);
@@ -15,6 +22,7 @@ class MovingPoint{
   constructor(x, y){
     this.x = x;
     this.y = y;
+    this.radius = 15;
     this.color = color(random(255), random(255), random(255));
     this.timeX = random(1000);
     this.timeY = random(1000);
@@ -24,13 +32,13 @@ class MovingPoint{
   display(){
     noStroke();
     fill(this.color);
-    circle(this.x, this.y, 10);
+    circle(this.x, this.y, this.radius);
   }
   update(){
     let dx = noise(this.timeX);
-    this.dx = map(dx,0, 1, -5, 5);
+    this.dx = map(dx, 0, 1, -5, 5);
     let dy = noise(this.timeY);
-    this.dy = map(dx,0, 1, -5, 5);
+    this.dy = map(dy, 0, 1, -5, 5);
     this.x += this.dx;
     this.y += this.dy;
     this.timeX += this.deltaTime;
@@ -47,11 +55,23 @@ class MovingPoint{
     if(this.y > height){
       this.y -= height;
     }
+    let mouseDist = dist(this.x, this.y, mouseX, mouseY);
+    if (mouseDist < this.reach){
+      let theSize = map(mouseDist, 0, this.reach, 30, 15);
+      this.radius = theSize;
+    }
+    else{
+      this.radius = 15;
+    }
   }
   connectTo(pointsArray){
     for(let otherPoint of pointsArray){
       if(this !== otherPoint){
-        
+        if (dist(this.x, this.y, otherPoint.x, otherPoint.y) <
+             this.reach) {
+          stroke(this.color);
+          line(this.x, this.y, otherPoint.x, otherPoint.y);
+        }
       }
     }
   }
